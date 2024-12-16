1d7976238b2d1e2633ef063fd9b3eafa24c0e0df
Quoc Hoang <hoangvanquoc923@gmail.com>
2024-12-16 18:05:22 +0700

    add github workflow

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello Dockerize App" }');
+app.get("/", function (req, res) {
+  res.send('{ "response": "Hello Dockerize App" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+app.get("/will", function (req, res) {
+  res.send('{ "response": "Hello World" }');
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+app.get("/ready", function (req, res) {
+  res.send('{ "response": " Great!, It works!" }');
 });
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`App is listennign in port ${port}`);
+});
+
 module.exports = app;
